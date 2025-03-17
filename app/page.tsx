1dceadf55ad92575f7deb170f22ea14c38604bdf
Viktor Lofgren <johan.viktor.lofgren@live.com>
2025-03-17 13:52:16 +0100

    Added css module for homepage and added search input and filter select in home page form

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,24 @@
 import RecipeCard from "./Components/RecipeCard";
 import fetchRecipes from "./Scripts/fetch";
+import styles from "./home.module.css";
 
 export default async function Home() {
 
   const data = await fetchRecipes();
   return (
-    <main>
-      <section>
+    <main className={styles.main}>
+      <section className={styles.searchAndFilterSection}>
         <form>
           <label htmlFor="mealName">Meal name:</label>
-          <input></input>
+          <input type="text" name="mealName" id="mealName" placeholder="Ex. Vegetarian Stir-Fry"></input>
+          <label htmlFor="filterSelect" className={styles.filterLabel}>Filter</label>
+          <select>
+            <option>filter by name</option>
+            <option>filter by tag</option>
+            <option>filter by mealtype</option>
+            <option>filter by difficulty</option>
+            <option>filter by cuisine</option>
+          </select>
         </form>
       </section>
       <ul>
