b26e2e5753b26b1e42352ff06c9213450b9bfa91
Alice <sten123465@gmail.com>
2025-03-17 15:29:19 +0100

    add- updateRecipeData function

--- app/Scripts/fetch.tsx
+++ app/Scripts/fetch.tsx
@@ -3,6 +3,29 @@ let recipeCache: Recipe[] = [];
 let lastUpdate: number = 0;
 const MAX_AGE: number = 3600000; //1 hour in ms
 const API_ENDPOINT: string = "https://dummyjson.com/recipes";
+//Will update the recipe data if the current data is old or missing.
+async function updateRecipeData() {
+  if(((Date.now() - lastUpdate) > MAX_AGE ) && (lastUpdate !== 0))
+    return;
+  let complete: boolean = false;
+  let recipeCounter = 0;
+  const recipes: Recipe[] = [];
+  while(!complete){
+    const response: RecipeResponse | null = await getPage(recipeCounter);
+    if (response === null)
+    {
+      console.error("Something went wrong with the file updating");
+      return;
+    }
+    recipeCounter += response.limit;
+    response.recipes.map((r) => {recipes.push(r)});
+    if(response.total === (response.skip + response.limit))
+      complete = true;
+  }
+  lastUpdate = Date.now();
+  recipeCache = recipes;
+}
+
 async function getPage(current: number): Promise<RecipeResponse | null>{
   console.log(current);
   try {
