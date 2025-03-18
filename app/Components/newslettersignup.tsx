c0214f419b27756da5999aa50bedc86f9b5251ab
Mikael Byström <mikaelb-frontend@proton.me>
2025-03-18 14:00:45 +0100

    footer and placeholder signup-form exist

---
+++ app/Components/newslettersignup.tsx
@@ -0,0 +1,12 @@
+export default function NewsletterSignup() {
+
+    return (
+        <>
+            <h3>Sign up for new recipes</h3>
+            <form>
+                <label htmlFor="email-newsletter"></label>
+                <input id="email-newsletter" name="email-newsletter" />
+            </form>
+        </>
+    )
+}
