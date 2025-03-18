fc69bd4ca5e44dd138af23005d805a4c213ebe5a
Viktor Lofgren <johan.viktor.lofgren@live.com>
2025-03-18 13:01:42 +0100

    Added Lucide Icons and changed some styling. Moved HTML Form to a component.

--- app/Components/recipecard.tsx
+++ app/Components/recipecard.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { Recipe } from "../Interfaces/interfaces";
 import styles from "./recipecard.module.css"
+import { CookingPot, CircleGauge, Utensils, Star, MessageSquare, Tag } from "lucide-react";
 
 export interface RecipeData{
   data: Recipe
@@ -20,18 +21,18 @@ function RecipeCard({ data }: RecipeData) {
           />
         </figure>
         <h2 className={styles.recipeCardTitle}>{data.name}</h2>
-        <h3 className={styles.recipeCardRating}>Rating: {data.rating}/5 Reviews: {data.reviewCount} </h3>
+        <h3 className={styles.recipeCardRating}>{data.rating}<Star size={16} fill="yellow" color="yellow"/> <MessageSquare size={16}/> {data.reviewCount} </h3>
         <section className={styles.recipeCardTagWrapper}>
-          <h3 className={styles.recipeCardTagHeading}>Tags:</h3>
+          <h3 className={styles.recipeCardTagHeading}><Tag size={16}/></h3>
           {data.tags.map((tag) => <h4 className={styles.recipeCardTag} key={tag}>{tag}</h4>)}
         </section>
         <section className={styles.recipeCardMealWrapper}>
           <h3 className={styles.recipeCardMealHeading}>Meal Type:</h3>
           {data.mealType.map((type) => <h4 className={styles.recipeCardMeal} key={type}>{type}</h4>)}
         </section>
-        <p className={styles.recipeCardPrepTime}>Cook time: {data.prepTimeMinutes + data.cookTimeMinutes}min</p>
-        <p className={styles.recipeCardDifficulty}>Diffulty: {data.difficulty}</p>
-        <p className={styles.recipeCardCuisine}>Cuisine: {data.cuisine}</p>
+        <p className={styles.recipeCardCookTime}><CookingPot/> {data.prepTimeMinutes + data.cookTimeMinutes}min</p>
+        <p className={styles.recipeCardDifficulty}>Difficulty: <CircleGauge size={20} color={data.difficulty == "Easy"? "green" : (data.difficulty == "Medium" ? "yellow" : "red")}/></p>
+        <p className={styles.recipeCardCuisine}><Utensils size={20}/> {data.cuisine}</p>
       </div>
     </Link>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,14 @@
 import RecipeCard from "./Components/recipecard";
 import fetchRecipes from "./Scripts/fetch";
 import styles from "./home.module.css";
+import SearchAndFilter from "./Components/searchandfilter";
 
 export default async function Home() {
 
   const data = await fetchRecipes();
   return (
     <main className={styles.main}>
-      <section className={styles.searchAndFilterSection}>
-        <form>
-          <label htmlFor="mealName">Meal name:</label>
-          <input type="text" name="mealName" id="mealName" placeholder="Ex. Vegetarian Stir-Fry"></input>
-          <label htmlFor="filterSelect" className={styles.filterLabel}>Filter</label>
-          <select>
-            <option>filter by name</option>
-            <option>filter by tag</option>
-            <option>filter by mealtype</option>
-            <option>filter by difficulty</option>
-            <option>filter by cuisine</option>
-          </select>
-        </form>
-      </section>
+      <SearchAndFilter />
       <ul>
         <li>{data?.map((recipe) => <RecipeCard key={recipe.id} data={recipe}/>)}</li>
       </ul>
