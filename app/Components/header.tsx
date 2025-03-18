0b6b3954c1998602309b0379db16dd7da856e8ee
AgnetaA <agnetaaxelsson@hotmail.se>
2025-03-18 16:48:01 +0100

    hamburger menu...

--- app/Components/header.tsx
+++ app/Components/header.tsx
@@ -5,7 +5,26 @@ export default function Header() {
 
     return (
         <header className={styles.header}>
-            <h1>Titel?</h1>
+            <h1>Welcome to our recipe collection</h1>
+
+
+
+
+            {/* TESTING!!!  */}
+            <div className={styles.smallMenu}>
+                <input className={styles.sideMenu} type="checkbox" id="side-menu" />
+                <label className={styles.hamb} htmlFor="side-menu">
+                    {/* Span förvandlas till meny-linjerna */}
+                    <span className={styles.hambLine}></span> 
+                </label>
+                
+                <nav className={styles.nav}><NavigationLinks /></nav>
+            </div>
+
+
+
+
+
 
             <nav className={styles.navMenu}>
                 <NavigationLinks />
