8a8401b51e9d232ee448d1c4c9fc262a35486d9d
Alice <sten123465@gmail.com>
2025-03-19 13:34:50 +0100

    added- id page base

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,4 +1,21 @@
+import { Recipe } from "../Interfaces/interfaces";
+import { fetchRecipe } from "../Scripts/fetch";
+import TagBubble from "../Components/tagbubble";
+import Image from "next/image";
+import styles from "./page.module.css";
+
 export default async function Page({ params }: { params: Promise<{ id: number }> }) {
     const id: number = (await params).id;
-    return <h1>Recipe id: {id}</h1>
+    const activeRecipe: Recipe = await fetchRecipe(id);
+    return (<main className={styles.mainPage}>
+        <header>
+            <Image
+            src={activeRecipe.image}
+            alt={`an image of ${activeRecipe.name}`}
+            width={400}
+            height={400}/>
+            <h1>{activeRecipe.name}</h1>
+            <ul className={styles.tagList}>{activeRecipe.mealType.map((mealType) => <TagBubble tagName={mealType} key={mealType}/>)}</ul>         
+        </header>
+    </main>);
 }
