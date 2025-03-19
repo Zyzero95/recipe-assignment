637eafd56e47b32247d857d415117e94c9f6747c
AgnetaA <agnetaaxelsson@hotmail.se>
2025-03-19 13:28:18 +0100

    adjustments

--- app/Components/header.tsx
+++ app/Components/header.tsx
@@ -5,27 +5,20 @@ export default function Header() {
 
     return (
         <header className={styles.header}>
-            <h1>Welcome to our recipe collection</h1>
+            <h1>The Fabulous Recipe Collection</h1>
 
-
-
-
-            {/* TESTING!!!  */}
+            {/* Mobile menu */}
             <div className={styles.smallMenu}>
                 <input className={styles.sideMenu} type="checkbox" id="side-menu" />
                 <label className={styles.hamb} htmlFor="side-menu">
-                    {/* Span förvandlas till meny-linjerna */}
+                    {/* Span becomes "burge lines" */}
                     <span className={styles.hambLine}></span> 
                 </label>
-                
+
                 <nav className={styles.nav}><NavigationLinks /></nav>
             </div>
 
-
-
-
-
-
+            {/* Desktop menu */}
             <nav className={styles.navMenu}>
                 <NavigationLinks />
             </nav>
