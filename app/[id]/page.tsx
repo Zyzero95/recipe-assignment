36cd98e1cf77c4171d726ff108f5c1d47d6740e7
Alice <sten123465@gmail.com>
2025-03-21 13:36:35 +0100

    added- more cards onto page

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -3,19 +3,43 @@ import { fetchRecipe } from "../Scripts/fetch";
 import TagBubble from "../Components/tagbubble";
 import Image from "next/image";
 import styles from "./page.module.css";
+import { ClockFading, Star } from "lucide-react";
+import CheckBoxList from "../Components/checkBoxList";
 
 export default async function Page({ params }: { params: Promise<{ id: number }> }) {
     const id: number = (await params).id;
     const activeRecipe: Recipe = await fetchRecipe(id);
-    return (<main className={styles.mainPage}>
-        <header>
-            <Image
-            src={activeRecipe.image}
-            alt={`an image of ${activeRecipe.name}`}
-            width={400}
-            height={400}/>
-            <h1>{activeRecipe.name}</h1>
-            <ul className={styles.tagList}>{activeRecipe.mealType.map((mealType) => <TagBubble tagName={mealType} key={mealType}/>)}</ul>         
+    return (<>
+        <header className={styles.flexLine}>
+            <div className={styles.imageCard}>
+                <Image
+                src={activeRecipe.image}
+                alt={`an image of ${activeRecipe.name}`}
+                width={500}
+                height={500}/>
+            </div>
+            <aside className={styles.textCard}>
+                <h1>{activeRecipe.name}</h1>
+                <ul className={styles.tagList}>
+                    {activeRecipe.mealType.concat(activeRecipe.tags).map((mealType) => 
+                    <TagBubble tagName={mealType} key={mealType}/>)}
+                </ul>
+                <h2><Star fill="var(--brown)" color="var(--brown)"/>{`${activeRecipe.rating}(${activeRecipe.reviewCount})`}</h2>
+                <h2>{activeRecipe.difficulty} recipe</h2>
+                <h2><ClockFading/>{`${activeRecipe.cookTimeMinutes + activeRecipe.prepTimeMinutes} minutes`}</h2>
+            </aside>
         </header>
-    </main>);
+        <main className={styles.flexLine}>
+            <section className={styles.textCard}>
+                <ul>
+                    <CheckBoxList items={activeRecipe.ingredients}/>
+                </ul>
+            </section>
+            <section className={styles.textCard}>
+                <ol>
+                    <CheckBoxList items={activeRecipe.instructions}/>
+                </ol>
+            </section>
+        </main>
+    </>);
 }
