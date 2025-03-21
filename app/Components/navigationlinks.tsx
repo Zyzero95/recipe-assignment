60249128cf253432ebab7ff39f2ed6b0e6310186
AgnetaA <agnetaaxelsson@hotmail.se>
2025-03-21 17:07:00 +0100

    get random number and push to link

--- app/Components/navigationlinks.tsx
+++ app/Components/navigationlinks.tsx
@@ -1,11 +1,38 @@
+"use client";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function NavigationLinks() {
+
+
+  const router = useRouter();
+
+  const randomNr: string = "";
+
+
+  //e.preventDefault();
+  // const number: number = Math.floor(Math.random() * 50) + 1;
+  // const randomNr: string = number.toString();
+  // console.log("RANDOM", randomNr);
+
+  const Randomize = (e: any) => {
+    e.preventDefault();
+    const number: number = Math.floor(Math.random() * 50) + 1;
+    const randomNr: string = number.toString();
+    console.log("RANDOM", randomNr);
+
+    router.push(randomNr);
+
+  }
+  // random = Math.floor(Math.random() * 50) +1;
+
+
+
   return (
     <ul>
       <li><Link href="/">Home</Link></li>
       <li><Link href="/about">About</Link></li>
-      <li><Link href="/search">Search</Link></li>
+      <li><Link href={randomNr} onClick={Randomize}>Surprise me!</Link></li>
     </ul>
   )
 }
