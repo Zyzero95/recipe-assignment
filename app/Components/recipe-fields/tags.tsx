2596d020d3a5a68da9afa2ddeccbd2c1da052796
Alice <sten123465@gmail.com>
2025-03-25 13:23:09 +0100

    changed- color of tags

--- app/Components/recipe-fields/tags.tsx
+++ app/Components/recipe-fields/tags.tsx
@@ -4,8 +4,8 @@ import TagBubble from "../tagbubble";
 export default function Tags({data}: {data: Recipe}){
     let tagCount: number = 1;
     return(<>
-        <TagBubble tagName={data.cuisine} key={`recipe${data.id}-tag-cuisine`} color="var(--dark-red)"/>
-        {data.mealType.map((r) => <TagBubble tagName={r} key={`recipe${data.id}-tag-${tagCount++}`} color="var(--beige)"/>)}
-        {data.tags.map((r) => <TagBubble tagName={r} key={`recipe${data.id}-tag-${tagCount++}`} color="var(--dark-charcoal)"/>)}
+        <TagBubble tagName={data.cuisine} key={`recipe${data.id}-tag-cuisine`} color="var(--tag-color-cuisine)"/>
+        {data.mealType.map((r) => <TagBubble tagName={r} key={`recipe${data.id}-tag-${tagCount++}`} color="var(--tag-color-meal)"/>)}
+        {data.tags.map((r) => <TagBubble tagName={r} key={`recipe${data.id}-tag-${tagCount++}`} color="var(--tag-color-default)"/>)}
     </>)
 }
