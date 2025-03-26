ae3eb5104101bb9bf97c3ed7a2b4df1b745ab6c8
Viktor Lofgren <johan.viktor.lofgren@live.com>
2025-03-26 13:08:09 +0100

    Created and implemented Preptime Component, fixed some styling (CSS classname component prop up for debate.)

--- app/Components/recipe-fields/difficulty.tsx
+++ app/Components/recipe-fields/difficulty.tsx
@@ -1,9 +1,10 @@
 import { Recipe } from "@/app/Interfaces/interfaces";
-import styles from "../recipecard.module.css"
+import homeStyle from "../recipecard.module.css"
+import idStyle from "../../[id]/page.module.css"
 import { SignalHigh, SignalMedium} from "lucide-react";
 
 
-export default function Difficulty({ data }: {data: Recipe}) {
-  return (<p className={styles.recipeCardDifficulty}>Difficulty: {data.difficulty} {data.difficulty === "Easy" ? 
+export default function Difficulty({ data, cssClass }: {data: Recipe, cssClass: string}) {
+  return (<p className={cssClass === "recipeCardDifficulty" ? homeStyle.recipeCardDifficulty : idStyle.cssClass}>Difficulty: {data.difficulty} {data.difficulty === "Easy" ? 
   <SignalMedium viewBox="0 -3 24 24" color="green"/> : <SignalHigh viewBox="0 -3 24 24" color="yellow"/>}</p>)
 }
