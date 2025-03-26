96f5f535fb09bea240e023b6b3623bc139cd9cce
Viktor Lofgren <johan.viktor.lofgren@live.com>
2025-03-26 13:39:49 +0100

    Created Pagination component and started implementing. Waiting for discussion about API fetch function

---
+++ app/Components/pagination.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react"
+
+export default function Pagination() {
+    const [currentPage, setCurrentPage] = useState(1);
+  return (
+    <div>
+      
+    </div>
+  )
+}
