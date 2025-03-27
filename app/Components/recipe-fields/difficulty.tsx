47b26f982b76a42d73245a389520237d00896f75
Viktor Lofgren <johan.viktor.lofgren@live.com>
2025-03-27 14:56:43 +0100

    Fixed components to not use css classname as prop. Instead wrap with a Section. Claned Recipe Card css module.

--- app/Components/recipe-fields/difficulty.tsx
+++ app/Components/recipe-fields/difficulty.tsx
@@ -1,10 +1,8 @@
 import { Recipe } from "@/app/Interfaces/interfaces";
-import homeStyle from "../recipecard.module.css"
-import idStyle from "../../[id]/page.module.css"
 import { SignalHigh, SignalMedium} from "lucide-react";
 
 
-export default function Difficulty({ data, cssClass }: {data: Recipe, cssClass: string}) {
-  return (<p className={cssClass === "recipeCardDifficulty" ? homeStyle.recipeCardDifficulty : idStyle.cssClass}>Difficulty: {data.difficulty} {data.difficulty === "Easy" ? 
+export default function Difficulty({ data }: {data: Recipe}) {
+  return (<p>Difficulty: {data.difficulty} {data.difficulty === "Easy" ? 
   <SignalMedium viewBox="0 -3 24 24" color="green"/> : <SignalHigh viewBox="0 -3 24 24" color="yellow"/>}</p>)
 }
--- app/Components/recipecard.tsx
+++ app/Components/recipecard.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 import Image from "next/image";
 import { Recipe } from "../Interfaces/interfaces";
 import styles from "./recipecard.module.css"
-import { Utensils, Star, MessageSquare, Tag } from "lucide-react";
 import Difficulty from "./recipe-fields/difficulty";
 import Preptime from "./recipe-fields/preptime";
 import Title from "./recipe-fields/title";
 import Tags from "./recipe-fields/tags";
+import Ratings from "./recipe-fields/rating";
 
 export interface RecipeData{
   data: Recipe
@@ -27,9 +27,9 @@ function RecipeCard({ data }: RecipeData) {
         </figure>
         <section className={styles.recipeCardTitle}><Title data={data}/></section>
         <section className={styles.recipeCardTags}><Tags data={data}/></section>
-        <section className={styles.recipeCardRating}><p className={styles.recipeCardRating}>{data.rating}<Star size={16} fill="yellow" color="yellow"/>{data.reviewCount}<MessageSquare size={16}/></p></section>
-        <Difficulty data={data} cssClass="recipeCardDifficulty"/>
-        <Preptime data={data} cssClass="recipeCardCookTime"/>
+        <section className={styles.recipeCardRating}><Ratings data={data}/></section>
+        <section className={styles.recipeCardDifficulty}><Difficulty data={data}/></section>
+        <section className={styles.recipeCardCookTime}><Preptime data={data}/></section>
       </section>
     </Link>
   )
