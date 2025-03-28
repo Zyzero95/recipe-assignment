ac719d8313bd50a511e8a90fcc8bb8f351ddd33b
Alice <sten123465@gmail.com>
2025-03-28 07:58:57 +0100

    changed- Calories to Paragraph

--- app/Components/recipe-fields/calorie.tsx
+++ app/Components/recipe-fields/calorie.tsx
@@ -2,5 +2,5 @@ import { Recipe } from "@/app/Interfaces/interfaces";
 import { Utensils } from "lucide-react";
 
 export default function Calories({data}: {data: Recipe}){
-    return (<h3><Utensils height="20px" width="20px"/>{`${data.caloriesPerServing} calories per serving`}</h3>)
+    return (<p><Utensils height="20px" width="20px"/>{`${data.caloriesPerServing} calories per serving`}</p>)
 }
